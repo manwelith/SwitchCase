651251ea70cd794ca6158e473c6f7a8611bb2917
Soner A <107695479+manwelith@users.noreply.github.com>
2022-07-14 17:02:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gunler = { "pazartesi", "salı", "carsamba", "persembe", "cuma", "cumartesi", "pazar" };
+
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 1:
+                    {
+                        Console.WriteLine(gunler[0]);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine(gunler[1]);
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine(gunler[2]);
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine(gunler[3]);
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine(gunler[4]);
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine(gunler[5]);
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine(gunler[6]);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Error.");
+                        break;
+                    }
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
